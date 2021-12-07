b05bb8ca4abe40b99edf19bff58b3930381ceb53
Allan Barcelos <albarsil@gmail.com>
2021-12-07 10:01:46 -0300

    Changed the way algorithms are initalized; Made more easy to create and instantiate classes

--- example/sklearn_example.py
+++ example/sklearn_example.py
@@ -4,9 +4,10 @@ sys.path.append('../geneticml/')
 
 import warnings
 from geneticml.optimizers import GeneticOptimizer
-from geneticml.strategy import EvolutionaryOptimizer
-from estimators import SklearnMLPEstimator
+from geneticml.strategy import EvolutionaryStrategy
+from geneticml.algorithms import EstimatorBuilder
 from metrics import metric_accuracy
+from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import load_iris
 from sklearn.exceptions import UndefinedMetricWarning, ConvergenceWarning
 
@@ -14,11 +15,20 @@ warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 
+def fit(model, x, y):
+    return model.fit(x, y)
+
+
+def predict(model, x):
+    return model.predict(x)
+
+
 if __name__ == "__main__":
 
     generations = 10  # Number of times to evole the population.
     population = 10  # Number of networks in each generation.
 
+    # Define a set of parameters that could be tested
     parameters = {
         "hidden_layer_sizes": [(10), (10, 10,)],
         "activation": ["relu"],
@@ -45,24 +55,34 @@ if __name__ == "__main__":
         "max_fun": [15000],
     }
 
-    strategy = EvolutionaryOptimizer(member_class=SklearnMLPEstimator, parameters=parameters, retain=0.4, random_select=0.1, mutate_chance=0.2, max_children=2)
+    # Creates an estimator
+    estimator = EstimatorBuilder().of(model_type=MLPClassifier).fit_with(func=fit).predict_with(func=predict).build()
 
-    optimizer = GeneticOptimizer(optimizer=strategy)
+    # Defines a strategy for the optimization
+    strategy = EvolutionaryStrategy(estimator_type=estimator, parameters=parameters, retain=0.4, random_select=0.1, mutate_chance=0.2, max_children=2)
 
+    # Creates the optimizer
+    optimizer = GeneticOptimizer(strategy=strategy)
+
+    # Loads the data
     data = load_iris()
-    
+
+    # Defines the metric
+    metric = metric_accuracy
     greater_is_better = True
 
+    # Create the simulation using the optimizer and the strategy
     models = optimizer.simulate(
         data=data.data, 
         target=data.target,
         generations=generations,
         population=population,
-        evaluation_function=metric_accuracy,
+        evaluation_function=metric,
         greater_is_better=greater_is_better,
         verbose=True
     )
 
+    # Print the results
     print([
         1 - x.fitness if greater_is_better else x.fitness
         for x in models
--- geneticml/algorithms.py
+++ geneticml/algorithms.py
@@ -1,4 +1,67 @@
-from abc import abstractmethod
+from __future__ import annotations
+from typing import Callable
+
+
+class EstimatorBuilder(object):
+    """
+    Examples
+    --------
+
+    estimator = EstimatorBuilder.of(class).parameters(dict).fit_with(fit_func).predict_with(predict_func).build()
+    
+    """
+
+    def of(self, model_type) -> EstimatorBuilder:
+        """
+        Assign a model type for the estimator
+
+        Parameters:
+            model_type (?): A model type
+
+        Returns:
+            (EstimatorBuilder): The current object
+        """
+
+        self._model_type = model_type
+        return self
+
+    def fit_with(self, func: Callable) -> EstimatorBuilder:
+        """
+        Define a function that will be used for the model training
+
+        Parameters:
+            func (Callable): A fit function used for model training
+
+        Returns:
+            (EstimatorBuilder): The current object
+        """
+
+        self._fit = func
+        return self
+
+    def predict_with(self, func: Callable) -> EstimatorBuilder:
+        """
+        Define a function that will be used for the model inference
+
+        Parameters:
+            func (Callable): A predict function used for model inference
+
+        Returns:
+            (EstimatorBuilder): The current object
+        """
+
+        self._predict = func
+        return self
+
+    def build(self) -> BaseEstimator:
+        """
+        Creates an instance of BaseEstimator
+
+        Returns:
+            (BaseEstimator): An instance of BaseEstimator that will be used for the optimization
+        """
+
+        return BaseEstimator(model_type=self._model_type, fit_func=self._fit, predict_func=self._predict)
 
 
 class BaseEstimator(object):
@@ -7,17 +70,41 @@ class BaseEstimator(object):
     A base class to be used for model optimization
     """
 
-    def __init__(self, model, parameters: dict) -> None:
+    def __init__(self, model_type, fit_func: Callable, predict_func: Callable) -> None:
         """
         Create a class instance
 
         Parameters:
-            instance (?): A model class instance
-            parameters (dict): Possible model paremters
+            model_type (?): A model type
+            parameters (dict): Possible model parameters
+            fit_func (Callable): A fit function used for model training
+            predict_func (Callable): A predict function used for model inference
         """
-        self._parameters = parameters
-        self._model = model(**parameters)
+        
+        self._parameters = None
+        self._model = None
+        self._model_type = model_type
+        self._fit_func = fit_func
+        self._predict_func = predict_func
         self._fitness = -1
+
+    def initialize(self, parameters: dict) -> BaseEstimator:
+        """
+        Create a class instance
+
+        Parameters:
+            parameters (dict): Possible model parameters
+        Returns:
+            (BaseEstimator): The current object with the model initialized
+        """
+
+        self._parameters = parameters
+        self._model = self._model_type(**self._parameters)
+        return self
+
+    @property
+    def model_type(self):
+        self._model_type
 
     @property
     def parameters(self) -> dict:
@@ -32,7 +119,7 @@ class BaseEstimator(object):
     @property
     def model(self):
         """
-        Property to access the model
+        Property to create a model from the model type and parameters
 
         Returns:
             (?): The base model
@@ -61,7 +148,6 @@ class BaseEstimator(object):
         """
         self._fitness = value
 
-    @abstractmethod
     def fit(self, x, y) -> None:
         """
         Method that performs model training. The inheriting class must implement this method.
@@ -70,14 +156,16 @@ class BaseEstimator(object):
             x (?): The data set
             y (?): The correct label set
         """
-        pass
+        self._model = self._fit_func(self._model, x, y)
 
-    @abstractmethod
-    def predict(self, x) -> None:
+    def predict(self, x) -> list:
         """
         Method that performs model inference. The inheriting class must implement this method.
 
         Parameters:
             x (?): The data set
+
+        Returns:
+            (list): The predict output
         """
-        pass
+        return self._predict_func(self._model, x)
